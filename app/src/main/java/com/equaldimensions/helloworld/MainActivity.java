de30adaf23683f9d65247ae94a8b21e43adfef06
equaldimensions <info@equaldimensions.com>
2021-11-11 09:18:13 +0530

    Test Commit

---
+++ app/src/main/java/com/equaldimensions/helloworld/MainActivity.java
@@ -0,0 +1,14 @@
+package com.equaldimensions.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
